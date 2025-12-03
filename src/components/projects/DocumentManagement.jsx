e2e76c4cbfc7b8bf6e39f5f7c97553d6b8d0d3bf
janick.kockartz <janick.kockartz@t-online.de>
2025-12-03 10:03:54 +0000

    File changes

--- src/components/projects/DocumentManagement.jsx
+++ src/components/projects/DocumentManagement.jsx
@@ -203,6 +203,7 @@ export default function DocumentManagement({ projectId, project, loadData }) {
     setShowSubfolderDialog(false);
     setNewSubfolderName("");
     setSelectedParentFolder("");
+    setShowUploadForm(true);
   };
 
   return (
