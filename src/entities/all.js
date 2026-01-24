8bd6a849520789a52b8abe8f9a51ebb3357cd9a6
Janick97 <janick.kockartz@as-tief-strassenbau.de>
2026-01-24 22:16:37 +0100

    Add Base44 entities stub to fix Vite build

--- src/entities/all.js
+++ src/entities/all.js
@@ -2,6 +2,12 @@
   Datei: all.js
   Zweck: Platzhalter-Datei, exportiert alle Entities.
   Generiert von GitHub Copilot auf Anfrage von Janick97 am 2026-01-24.
+
+  Die folgenden git-Befehle werden hier als Kommentar abgelegt:
+
+  git add src/entities/all.js
+  git commit -m "Add Base44 entities stub to fix Vite build"
+  git push
 */
 
 export default {};
