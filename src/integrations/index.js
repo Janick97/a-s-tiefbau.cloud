db55d1019369c3c2d69438288056ab8280c6583a
Janick97 <janick.kockartz@as-tief-strassenbau.de>
2026-01-24 22:22:28 +0100

    Add src/integrations folder with placeholder

---
+++ src/integrations/index.js
@@ -0,0 +1,7 @@
+/*
+  Datei: index.js
+  Zweck: Platzhalter in src/integrations, damit der Ordner im Repository angelegt wird.
+  Generiert von GitHub Copilot auf Anfrage von Janick97 am 2026-01-24.
+*/
+
+export default {};
