697236f19fd1a557e0d802e46f88859e921638ac
Janick97 <janick.kockartz@as-tief-strassenbau.de>
2026-01-24 22:38:34 +0100

    Delete src/integrations directory

--- src/integrations/index.js
+++
@@ -1,7 +0,0 @@
-/*
-  Datei: index.js
-  Zweck: Platzhalter in src/integrations, damit der Ordner im Repository angelegt wird.
-  Generiert von GitHub Copilot auf Anfrage von Janick97 am 2026-01-24.
-*/
-
-export default {};
